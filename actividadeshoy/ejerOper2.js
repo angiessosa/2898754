41a38d5d98dabe43eb614a607eafdb57cf7dbb6a
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-03-01 17:07:03 -0500

    Delete actividadeshoy directory

--- actividadeshoy/ejerOper2.js
+++
@@ -1,9 +0,0 @@
-console.log("2. Ingresar dos números por teclado y sumarlos: ");
-console.log("Ingrese número uno: ");
-let numuno=12;
-console.log("Ingrese número dos: ");
-let numdos=5;
-
-let resultado=(numuno+numdos);
-
-console.log(resultado);
