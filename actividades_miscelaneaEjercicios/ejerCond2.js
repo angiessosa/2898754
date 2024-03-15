153f6d90fa3b8265c71a77b5753183f4c7f1db04
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-03-15 17:04:08 -0500

    ejerCond2.js

--- actividades_miscelaneaEjercicios/ejerCond2.js
+++ actividades_miscelaneaEjercicios/ejerCond2.js
@@ -1,11 +1,13 @@
 console.log("2. Escribir un algoritmo que reciba dos números por teclado y diga cuál es el mayor y cuál el menor: ");
 console.log("Ingrese el primer número: ");
-let num1=56;
+let num1=78;
 console.log("Ingrese el segundo número: ");
-let num2=61;
+let num2=89;
 
 if (num1>num2){
     console.log("El número mayor es: ",num1," y el menor es: ",num2);
+}else if (num1==num2){
+    console.log("Los números ingresados son iguales");
 }else{
     console.log("El número mayor es: ",num2," y el menor es: ",num1);
 }
