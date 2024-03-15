1d1f5a34fa873f7ee77a283e59d253aa6fb2b2e7
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-03-15 17:10:14 -0500

    test

---
+++ actividades_miscelaneaEjercicios/sds.js
@@ -0,0 +1,45 @@
+//FOR
+//EScriba los numeros del 1 al 10
+for(/*desde*/let i=10; /*hasta*/i>0; i--){
+    console.log(i);
+}
+
+console.log("pares");
+//imprimir los pares de 1 a 10
+for(let i=0; i<11 ; i+=2){
+    console.log(i);
+}
+
+console.log("tercero");
+//tabla del 9
+for(let i=81; i>=0 ; i-=9){
+    console.log(i);
+}
+
+//definicion de objetos de javascript
+// const person={fname:"John", lname:"Doe", age:25};
+// []=números {}=objetos
+
+console.log("while");
+//WHILE
+let i=0;
+while(i<10){
+    i++;
+    console.log(i);
+}
+console.log("10 al 1");
+
+let ii=10;
+while(ii>0){
+    console.log(ii);
+    ii--;
+}
+
+//DO WHILE
+console.log("do while");
+let j=0;
+do{
+    console.log(j);
+    j+=2;
+}while(j<=10);
+
