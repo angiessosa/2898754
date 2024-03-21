25701f717f4e6ae2d899f469b68119a0b5ebbe06
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-03-20 19:02:15 -0500

    ejerCiclos4.js

--- actividades_miscelaneaEjercicios/ejerCiclos4.js
+++ actividades_miscelaneaEjercicios/ejerCiclos4.js
@@ -1,4 +1,4 @@
 console.log("4.Escribir un programa que imprima por pantalla los cuadrados de los números del 1 al 30.");
 for(let i=1; i<31 ; i++){
-    console.log(i*i);
+    console.log(i + " * " + i + " = " + (i * i));
 }
