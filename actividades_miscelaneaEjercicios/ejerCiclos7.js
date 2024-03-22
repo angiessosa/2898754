de4b07c8fbc3ee9831216e39f79b909d0e866f35
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-03-22 13:45:03 -0500

    Delete actividades_miscelaneaEjercicios/ejerCiclos7.js

--- actividades_miscelaneaEjercicios/ejerCiclos7.js
+++
@@ -1,8 +0,0 @@
-console.log("7. Sumar todos los números que se digitan por teclado mientras no sea cero, digite 0 para terminar el ciclo");
-let suma=0;
-let num;
-do{
-    num=parseInt(prompt("Ingrese un número: "));
-    suma=suma+num;
-}while (num!==0);
-console.log("La suma de los números ingresados es: ",suma);
